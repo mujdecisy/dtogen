d27354fa1f005d768fc570e8c29e973edba1c074
mujdecisy <mujdecisy@gmail.com>
2023-07-23 08:57:28 +0300

    chore: add find_packages to setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os.path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -8,7 +8,7 @@ with open(os.path.join(HERE, "README.md")) as fid:
 
 setup(
     name="dtogen",
-    version="0.0.5",
+    version="0.0.6",
     description="DTO generator for Java, TypeScript, Python",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -19,7 +19,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
-    packages=["dtogen"],
+    packages=find_packages(exclude=["test", "test.*"]),
     include_package_data=True,
     install_requires=["mapperr", "pyyaml"],
 )
