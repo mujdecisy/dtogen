13b01a64a5b2ed425eb4dda3da75c5acd86580e8
mujdecisy <mujdecisy@gmail.com>
2024-05-27 00:54:30 +0300

    feat: add realtions for java

--- dtogen/transformers/transformer_java.py
+++ dtogen/transformers/transformer_java.py
@@ -1,5 +1,5 @@
 from .transformer import __Transformer
-from ..interfaces import NestedAttribute
+from ..interfaces import NestedAttribute, Relation
 
 
 class TransformerJava(__Transformer):
@@ -17,6 +17,8 @@ class TransformerJava(__Transformer):
             "float": "Float",
             "string": "String",
             "boolean": "Boolean",
+            "class": "Class",
+            "object": "Object"
         }[type_name]
 
     def get_array_type(self) -> str:
@@ -27,3 +29,49 @@ class TransformerJava(__Transformer):
 
     def get_attribute_line(self, attribute_name: str, attribute_type: str) -> str:
         return f"    public {attribute_type} {attribute_name};"
+
+    def __create_literal(self, value: str, type: str) -> str:
+        keyval = value
+        if type == "string":
+            keyval = f'"{value}"'
+        elif type == "class":
+            keyval = self.get_class_name(value)
+        return keyval
+
+    def create_private_static_final_map_attr(self, relation: Relation) -> str:
+        e1c = self.get_primitive_type(relation.info.entity1.type)
+        e2c = self.get_primitive_type(relation.info.entity2.type)
+
+        text = f"    private static final Map<{e1c}, {e2c}> e1_e2 = new HashMap<>() {'{{'}\n"
+        text_reversed = f"    private static final Map<{e2c}, {e1c}> e2_e1 = new HashMap<>() {'{{'}\n"
+
+        for item in relation.attributes:
+            e1l = self.__create_literal(item.entity1, relation.info.entity1.type)
+            e2l = self.__create_literal(item.entity2, relation.info.entity2.type)
+
+            text += f"      put({e1l}, {e2l});\n"
+            text_reversed += f"      put({e2l}, {e1l});\n"
+
+        text += "   }};\n"
+        text_reversed += "  }};\n"
+
+        return text + text_reversed
+
+    def create_mapper_function(self, relation: Relation, class_name: str) -> str:
+        e1n = relation.info.entity1.name
+        e2n = relation.info.entity2.name
+        e1c = self.get_primitive_type(relation.info.entity1.type)
+        e2c = self.get_primitive_type(relation.info.entity2.type)
+
+        class_name = self.get_class_name(class_name)
+
+        text = f"""
+    public static {e2c} {e1n}_to_{e2n}({e1c} value) {'{'}
+        return {class_name}.e1_e2.get(value);
+    {'}'}
+
+    public static {e1c} {e2n}_to_{e1n}({e2c} value) {'{'}
+        return {class_name}.e2_e1.get(value);
+    {'}'}
+"""
+        return text
